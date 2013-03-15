d9173e577f86cdf812021a4a2db3b508ee0fb48c
Milagros Ramos <mirakel1401@gmail.com>
2013-03-15 06:29:07 -0500

    modificando modelo y controlador - inscriptos

--- admin/inscriptos/controlador.php
+++ admin/inscriptos/controlador.php
@@ -1,5 +1,17 @@
 <?php 
 	require_once('modelo.php');
 
+	if(isset($_GET['opcion'])){
+		if($_GET['opcion']=='eliminar'){
+			$id_inscripto=$_GET['id'];
+		
+			if(eliminar_inscripcion($id_inscripto)){
+				header("location:index.php?mensaje=1");
+			}
+			else{
+				header("location:index.php?mensaje=2");	
+			}
+		}
+	}
 	
 ?>
--- admin/inscriptos/modelo.php
+++ admin/inscriptos/modelo.php
@@ -1,4 +1,5 @@
 <?php 
-
-
+	function eliminar_inscripcion($id_inscripto){
+		return consulta("DELETE FROM inscriptos WHERE id = '".$id_inscripto."'");
+	}
 ?>

a12a39afee857bb9905810c9ca73d31cab4a3140
guidox18 <guido_x18@hotmail.com>
2013-04-23 21:47:18 -0500

    agregando versión live de la pagina

---
+++ index-live.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html lang="es-Es">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Festival latinoamericano de instalacion de Software libre 2013 - Flisol tacna">
+	<meta name="author" content="comunidad de software libre Basadrinux">
+	<link href='recursos/img/icono.png' rel='shortcut icon' type='image/gif'/>
+	<link rel="stylesheet" href="recursos/css/live.css">
+	<link href='http://fonts.googleapis.com/css?family=Advent+Pro' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Julius+Sans+One' rel='stylesheet' type='text/css'>	
+	<!--Cuenta regresiva-->
+	<link type="text/css" href="recursos/js/jquery.countdown.css" rel="stylesheet" />
+	<script type="text/javascript" src="recursos/js/ext/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="recursos/js/jquery.countdown.js"></script>
+	<script type="text/javascript" src="recursos/js/jquery.countdown-es.js"></script>
+
+	<title>Flisol Tacna</title>
+</head>
+<body>
+<div id="contenedor">	
+	<header>
+		<div id="titulo">
+			<h1>9no Festival Latinoamericano de Instalación de Software Libre- Flisol Peru 2013</h1>
+		</div>		    
+		<div id="logo">
+			<img src="recursos/img/vivo.png">
+		</div>
+	</header>	
+	<div id="modulos">
+		<div id="video">
+			<div id="url">
+				<embed src="http://www.youtube-nocookie.com/v/ut5s3ESEL5Q?version=3&amp;hl=es_MX" type="application/x-shockwave-flash" width="100%" height="100%" allowscriptaccess="always" allowfullscreen="true">
+			</div>
+			<div class="publicidad">
+				<div id="redsocial">
+				<span>Síguenos en</span>
+				<a href="https://www.facebook.com/FlisolTacnaPeru" target="_blank"><img src="recursos/img/facebook-icon.png"></a>
+				<a href="https://twitter.com/flisoltacna" target="_blank"><img src="recursos/img/twitter-icon.png"></a>
+				<a href="https://www.youtube.com/user/FlisolTacnaPeru" target="_blank"><img src="recursos/img/youtube-icon.png"></a>
+				<a href="https://plus.google.com/u/0/111966957349242981203" target="_blank"><img src="recursos/img/google-icon.png"></a>
+				</div>
+			    <div id="info">
+				Festival Latinoamericano de Instalación de Software Libre (FLISoL) es el evento de difusión de Software Libre más grande en Latinoamérica. Se realiza desde el año 2005 y su principal objetivo es promover el uso del software libre, dando a conocer al público en general su filosofía, alcances, avances y desarrollo.
+				</div> 
+			</div>
+		</div>
+		<div id="chat">
+			<iframe scrolling="auto" src="http://www.ikphp.tk/#_=_" frameborder="0">
+			</iframe>
+		</div>
+		<div id="auspicios">
+					<a href="http://innovaeureka.com/"><img src="recursos/img/Patrocinan/eureka.png" ></a>
+					<a href="#"><img src="recursos/img/Patrocinan/chicha-company.png" ></a>
+					<a href="#"><img src="recursos/img/Patrocinan/tacna.gif" ></a>
+					<a href="#"><img src="recursos/img/Patrocinan/radio-cmf.png" ></a>
+					<a href="#"><img src="recursos/img/Patrocinan/logo-ebenezer.png"></a>
+					<a href="#"><img src="recursos/img/Patrocinan/coyotex.png"></a>
+					<a href="#"><img src="recursos/img/Patrocinan/SUPER_STEREO.png"></a>
+		</div>
+	</div>
+		
+</div>
+</body>
+</html>
